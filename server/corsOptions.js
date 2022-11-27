b1d79c577e4c1c181b52d45cc96eb473736e8d1b
Pillarofcats <jacob.white.07@gmail.com>
2022-11-27 15:55:38 -0600

    Added cookies

--- server/corsOptions.js
+++ server/corsOptions.js
@@ -1,6 +1,6 @@
 const corsOptions = {
   //Access-Control-Allow-Origin
-  origin: 'https://react-register-login-production.up.railway.app/*',
+  origin: 'https://react-register-login-production.up.railway.app',
   //Access-Control-Allow-Methods
   methods: ['GET','POST','HEAD'],
   //Access-Control-Allow-Credentials
