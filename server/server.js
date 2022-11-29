5c08699e51d769c37cfd9566a9dcca9004287df4
Pillarofcats <jacob.white.07@gmail.com>
2022-11-29 02:38:39 -0600

    Auth user

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 //Password encryption
 const bcryptjs = require('bcryptjs')
 //Encrypt/Decrypt session cookies
-const cryptojs = require('crypto-js/aes')
+const cryptojs = require('crypto-js')
 //Parse Cookies
 const cookieParser = require('cookie-parser')
 //Environment variable config
