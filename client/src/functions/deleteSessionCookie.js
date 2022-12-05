150ca9ea0af5086dd34215eb205b374275268088
Pillarofcats <jacob.white.07@gmail.com>
2022-12-04 18:43:42 -0600

    Auth user

--- client/src/functions/deleteSessionCookie.js
+++ client/src/functions/deleteSessionCookie.js
@@ -1,5 +1,7 @@
 function deleteSessionCookie () {
+  console.log('deleting...')
   document.cookie = "sessionID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+  console.log('deleted value', document.cookie)
 }
 
 export default deleteSessionCookie
