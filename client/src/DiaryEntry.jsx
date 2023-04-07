0a6d1e7600b25db76accf8b9146f5f5edbbc1284
Pillarofcats <jacob.white.07@gmail.com>
2023-04-06 23:44:17 -0500

    Diary feature in progress

--- client/src/DiaryEntry.jsx
+++ client/src/DiaryEntry.jsx
@@ -8,7 +8,8 @@ function DiaryEntry({entry, index}) {
 
   function deleteEntry(ind) {
 
-    console.log(user.id.id, ind)
+    console.log(user.user.id, ind, index)
+
     updateDiaryEntry(user.user.id, ind)
       .then((res) => {
         //If user response has .errMessage property set error message
